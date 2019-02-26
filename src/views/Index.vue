a9b008137d7a67d9861a484e1ce8bf308fd9a836
zengjielin <248717607@qq.com>
2019-02-26 17:01:13 +0800

    更新主页导航

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,43 +1,13 @@
 <template>
     <div class="index">
-        <!-- <strip-progress :config="config" :percent="percent" :gradient="gradient" :height='30'></strip-progress> -->
-        <!-- <circle-progress :percent="percent" :progressGradient="gradient" :circleRadius='100' :progressConfig='config'></circle-progress> -->
+        <router-link to="/v_switch">switch开关</router-link>
+        <router-link to="/v_circle_progress">圆形进度条</router-link>
+        <router-link to="/v_strip_progress">条形进度条</router-link>
     </div>
 </template>
 
 <script>
-import StripProgress from "@/components/strip-progress/StripProgress.vue";
-import CircleProgress from "@/components/circle-progress/CircleProgress.vue";
 export default {
-  data() {
-    return {
-      config: {
-        ringWidth: 6,
-        ringBackground: "#E5E9F2",
-        ringColor: "#20A0FF",
-        startGradient: "#adfc77",
-        endGradient: "#09c669"
-      },
-      percent: 0,
-      gradient: true,
-      // config: {
-      //   ringWidth: 16, //圆环宽度
-      //   ringBackground: "#E5E9F2", //进度条背景色
-      //   ringColor: "#5EE270", //进度条颜色
-      //   startGradient: "#adfc77", //开始渐变色
-      //   endGradient: "#09c669" //结束渐变色
-      // }
-    };
-  },
-  components: {
-    StripProgress,
-    CircleProgress
-  },
-  mounted() {
-    setTimeout(() => {
-      this.percent = 50;
-    }, 1000);
-  }
 };
 </script>
 
